9a99df4a0538992d56279fbb3f5449d867f88ea2
SONY-STRING <sctoa1@gmail.com>
2024-11-27 22:17:04 +0900

    refactor(App): App 컴포넌트를 `src/components` 디렉토리에 옮겼습니다.
    - App 컴포넌트를 `src/components` 디렉토리에 옮겨서 컴포넌트를 관리하기 용이하게 하였습니다.
    
    Co-authored-by: Kim Minseok <minseok@chungbuk.ac.kr>

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState, useRef } from 'react'
+import { Excalidraw, serializeAsJSON } from "@excalidraw/excalidraw";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default App;
