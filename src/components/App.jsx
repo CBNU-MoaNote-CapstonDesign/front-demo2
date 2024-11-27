ba6bc8ed737a12674c97aaf98458207de5515c38
SONY-STRING <sctoa1@gmail.com>
2024-11-27 22:24:38 +0900

    feat(TextBlock): `Editor` 의 하위 컴포넌트인 `TextBlock` 컴포넌트 추가
    - text 데이터를 입력할 수 있는 블록입니다.
    - 나중에 markdown 관련 라이브러리의 컴포넌트로 대체될 수 있습니다.
    - textarea 의 내용을 state 로 관리합니다. (`contents`)
    
    Co-authored-by: Kim Minseok <minseok@chungbuk.ac.kr>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,40 @@ import React from "react";
 import { useState, useRef } from 'react'
 import { Excalidraw, serializeAsJSON } from "@excalidraw/excalidraw";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+function TextBlock({id, initialContents, hookContentsUpdate}) {
+  const [contents, setContents] = useState(initialContents);
+  const textareaRef = useRef(null);
+
+  const handleInput = (event) => {
+    const textarea = textareaRef.current;
+    if (textarea) {
+      textarea.style.height = "auto";  // CSS 높이를 초기화
+      textarea.style.height = `${textarea.scrollHeight}px`;  // 컨텐츠 높이에 맞게 조정
+    }
+    setContents(event.target.value);  // 상태 업데이트
+  };
+
+
+  function handleContentsUpdate(e) {
+    setContents(e.target.value);
+    hookContentsUpdate(id, e.target.value);
+  }
+
+  return (
+    <>
+      <textarea 
+        ref={textareaRef}
+        type="text"
+        className="form-control"
+        id={id} 
+        value={contents} 
+        onChange={handleContentsUpdate}
+        onInput={handleInput}
+      />
+    </>
+  )
+}
 function Editor() {
   let [docBlocks, setDocBlocks] = useState([]);
 
