4f51f62d374ddaa39059ccafbdd260a9c1d54c05
SONY-STRING <sctoa1@gmail.com>
2024-11-27 22:21:33 +0900

    feat(Editor): 문서 수정을 위한 인터페이스인 `Editor` 컴포넌트 추가
    - 문서 수정을 위한 인터페이스인 `Editor` 컴포넌트 추가하였습니다.
    - state 로 하위 컴포넌트를 관리하는 docBlocks 배열을 가집니다.
    - 전체 블록들의 raw data 를 저장하는 `docBlocksContents` 를 로컬 변수로 가집니다.
    - `docBlocksContents` 의 갱신은 각 `TextBlock` 과 `GraphBlock` 에서 이루어집니다.
    
    Co-authored-by: Kim Minseok <minseok@chungbuk.ac.kr>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,69 @@ import React from "react";
 import { useState, useRef } from 'react'
 import { Excalidraw, serializeAsJSON } from "@excalidraw/excalidraw";
 import "bootstrap/dist/css/bootstrap.min.css";
+function Editor() {
+  let [docBlocks, setDocBlocks] = useState([]);
+
+  let docBlocksContents = [];
+  
+  function addTextBlock() {
+      let newDocBlocks = [...docBlocks];
+      newDocBlocks.push(
+        {
+          isTextBlock : true,
+          id          : newDocBlocks.length,
+          initialContents        : '',
+        }
+      );
+      docBlocksContents.push('');
+    setDocBlocks(newDocBlocks);
+    console.log("Text Block 생성");
+  }
+
+  function addGraphBlock() {
+      let newDocBlocks = [...docBlocks];
+      newDocBlocks.push(
+        {
+          isTextBlock : false,
+          id          : newDocBlocks.length,
+          initialContents: '',
+        }
+      );
+      docBlocksContents.push('');
+      setDocBlocks(newDocBlocks);
+      console.log("Graph Block 생성");
+  }
+
+  function updateContents(idx, contents) {
+    docBlocksContents[idx] = contents;
+  }
+
+  function printContents() {
+    let allContents = '';
+    for (let i in docBlocksContents) {
+      allContents += docBlocksContents[i];
+    }
+    console.log(allContents);
+  }
+  
+  return (
+    <>
+      <Navbar handleAddTextBlock={addTextBlock} handleAddGraphBlock={addGraphBlock} handlePrintButtonClick={printContents} />
+      {
+        docBlocks.map((docBlock) => 
+          docBlock.isTextBlock ? 
+          <TextBlock id={docBlock.id} initialContents={docBlock.initialContents} hookContentsUpdate = {updateContents}/>
+          : <GraphBlock id={docBlock.id} />
+        )
+      }
+    </>
+  );
+}
+
 function App() {
   return (
     <>
+      <Editor />
     </>
   )
 }
